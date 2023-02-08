9cc090b6824e6932f65073dee61894614d5fd4e2
Motdep@sse <lirsitogo2021@gmail.com>
2023-02-08 09:20:56 +0000

    finish the auth model

--- src/web/controller/auth.controller.js
+++ src/web/controller/auth.controller.js
@@ -1,12 +1,13 @@
 "use strict";
 const jwt = require("jsonwebtoken");
-const { Noc } = require("../models/noc.model");
+const auth = require("../model/auth.model");
+const bcrypt = require("bcrypt");
 
 exports.auth = function (req, res) {
   console.log("[+] Controller Auth");
-  const email = req.query.email_noc;
-  const password = req.query.password_noc;
-
+  const email = req.body.email;
+  const password = req.body.password;
+  const passhash = bcrypt.hashSync(password, 10);
   const token = jwt.sign(
     {
       id: email,
@@ -14,11 +15,9 @@ exports.auth = function (req, res) {
     "jwtPrivateToken",
     { expiresIn: "5m" }
   );
-
   console.log("[+] Email: " + email);
   console.log("[+] Password: " + password);
-
-  Noc.auth(email, password, function (err, noc) {
+  auth(email, passhash, function (err, noc) {
     if (err) {
       res.send("[-] An error occured " + err);
       console.log("[-] An error occured " + err);
