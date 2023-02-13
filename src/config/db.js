e6f0552c224c73f563c68f8ca5f384926fb569f0
Motdep@sse <lirsitogo2021@gmail.com>
2023-02-13 10:50:23 +0000

    modified:   src/config/db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,28 @@
+require('dotenv').config();
 const mysql = require('mysql');
 
-var connect = mysql.createConnection({
-  host: "mysql-testdep.alwaysdata.net",
-  user: "testdep_useroot",
-  password: "Motdep@sse2022",
-  database: "testdep_energy",
+var connect = mysql.createPool({
+  host: process.env.DB_HOST || 'mysql-testdep.alwaysdata.net',
+  user: process.env.BD_USER || 'testdep_user',
+  password: process.env.BD_PASSWORD || 'Motdep@sse2022',
+  database: process.env.DB_NAME || 'testdep_noc',
+  queueLimit: 0,
+  waitForConnections: true,
+  connectionLimit: 50,
 });
 
-function verify() {
+/* var connect = mysql.createConnection({
+  host: process.env.DB_HOST || 'mysql-testdep.alwaysdata.net',
+  user: process.env.BD_USER || 'testdep_user',
+  password: process.env.BD_PASSWORD || 'Motdep@sse2022',
+  database: process.env.DB_NAME || 'testdep_noc', 
+}); */
+
+/* function verify() {
   connect.connect(function (err, conn) {
     if (err) return console.log("[-] AN error occurred :", JSON.stringify(err));
     else console.log("[+] Connected successfully ");
   });
-}
+} */
 
-module.exports = { verify: verify(), connect: connect };
+module.exports = {  connect: connect };
